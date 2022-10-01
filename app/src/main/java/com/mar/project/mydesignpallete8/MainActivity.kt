0e0d1944b35df5d6091dfd2075242cc066e7267d
Iffa <Mauliffarnesya@yahoo.com>
2022-10-02 06:58:52 +0700

    first commit

---
+++ app/src/main/java/com/mar/project/mydesignpallete8/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mar.project.mydesignpallete8
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
